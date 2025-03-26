4167581604024cdf10ee5b0903cb24d7fd7bc267
Dejam Mitrovic <mitrovic75122@gmail.com>
2025-03-26 12:27:01 +0900

    first commit

---
+++ api/index.js
@@ -0,0 +1,32 @@
+const cors = require("cors")
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const quizRoutes = require('../routes/quizRoute'); // Import quiz routes
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+
+// Middleware
+app.use(express.json());  // Parse incoming JSON requests
+
+// Register the quiz routes
+app.use('/api', quizRoutes);  // Make sure quizRoutes is the router, not an object
+
+// MongoDB connection
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected...'))
+.catch(err => console.log('MongoDB connection error:', err));
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
---
+++ models/Quiz.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const quizSchema = new mongoose.Schema(
+  {
+    question: {
+      type: String,
+      required: true,
+    },
+    answer: {
+      type: String, // The correct answer
+      required: true,
+    },
+    like: {
+        type: Number,
+        required: false,
+        default: 0
+    }
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  }
+);
+
+const Quiz = mongoose.model('Quiz', quizSchema);
+
+module.exports = Quiz;
